7a09b7fdf34fb14740ccd04dbd543263e18b986b
iliangood <ilia191168@gmail.com>
2026-08-01 05:19:25 +0300

    -

---
+++ config/inputs.lua
@@ -0,0 +1,20 @@
+-- Input configuration
+
+hl.config({
+	input = {
+		sensitivity = 0.6,
+		accel_profile = "flat",
+		kb_layout = "us,ru",
+		kb_options = "grp:alt_shift_toggle",
+	},
+	-- Uncomment the section below to enable software cursors; this can help with cursor display or behavior issues
+	cursor = {
+		-- no_hardware_cursors = 1,
+		-- inactive_timeout = 3,
+	},
+})
+
+hl.gesture({ fingers = 4, direction = "horizontal", action = "workspace" })
+hl.gesture({ fingers = 3, direction = "down", action = "close" })
+hl.gesture({ fingers = 3, direction = "up", action = "fullscreen" })
+hl.gesture({ fingers = 3, direction = "left", action = "float" })
---
+++ config/variables.lua
@@ -0,0 +1,16 @@
+-- Hyprland default apps
+
+TERMINAL = "kitty"
+FILE_MANAGER = "thunar"
+BROWSER = "google-chrome-stable --ozone-platform=wayland --enable-features=UseOzonePlatform,WaylandWindowDecorations"
+EDITOR = "gnome-text-editor --new-window"
+CALCULATOR = "gnome-calculator"
+
+-- Monitors
+MONITOR1 = ""
+MONITOR2 = ""
+MONITOR3 = ""
+PRIMARY_MONITOR = MONITOR1
+
+-- Workspaces
+NUM_WPM = 10 -- Number of workspaces per monitor (Max 10)
